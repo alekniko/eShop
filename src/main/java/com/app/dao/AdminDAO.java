c5632ceb4ec5ac81ecc78b3f3d17a1c0171d9f48
Aleksejs Nikolajevs <aleksejs.nikolajevs@gmail.com>
2024-01-25 19:31:24 +0200

    add save Client endpoint

---
+++ src/main/java/com/app/dao/AdminDAO.java
@@ -0,0 +1,11 @@
+package com.app.dao;
+
+import com.app.model.Admin;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AdminDAO {
+    public void storeNewUser(Admin admin) {
+
+    }
+}
